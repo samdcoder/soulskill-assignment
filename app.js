6bb95df80fce2fbda4ac742313c0a0096ca0d380
samdcoder <pathan.sameer3@gmail.com>
2018-09-20 12:43:55 +0530

    added server side .pdf file format check for resume

--- app.js
+++ app.js
@@ -3,12 +3,29 @@ const app = express();
 const path = require('path');
 const fs = require('fs');
 const bodyParser = require('body-parser');
+const upload = require('express-fileupload');
 
-app.use(express.static(__dirname+ '/public'));
+app.use(upload());
+app.use(express.static(__dirname+ '\\public'));
 app.use(bodyParser());
 
 app.get('/', function(request, response){
 	response.sendFile('index.html');
+});
+
+app.post('/', function(request, response){
+	if(request.files){
+		var file = request.files.filename;
+		var filename = file.name;
+		console.log(file.mimetype);
+		if(file.mimetype != 'application/pdf'){
+			console.log('Did not match!');
+			response.sendFile('pdf_check.html', {"root": path.join(__dirname, 'public')});
+		}
+
+		
+		
+	}
 });
 
 app.listen(3000, function(){
